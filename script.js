ee347637dc641d4e108e67213b0c341701c8c89a
Pedro Macedo <pedromacedo201@gmail.com>
2022-01-15 13:21:46 -0300

    Adicionar nova imagem a pagina html

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
 gerarValorAleatorio = () => {
-    return Math.floor(Math.random () * 671);
+    return Math.floor(Math.random() * 671);
 }
 
 pegarPersonagem = () => {
